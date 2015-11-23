Fixed normalization issue for calculated wavenumbers from FFT.
Fixed incorrect fft output due to nonuniform x; a signal sampled at
equally-spaced intervals is first formed using interp1d(kind='cubic')

Improved plotting.

post_wave_fft.py
@@ -3,18 +3,20 @@ import sys
 import os
 import numpy as np
 from fenton1985 import *
+from scipy.interpolate import interp1d
 from numpy import fft
-
-import matplotlib.pyplot as plt
 
 macro = 'setupSim.java'
 surfDir = 'waterSurface'
 
+periodsToCompare = 4
 g = 9.81    # gravity
+NSMOO = 10  # smoothing for zero-finding, don't need to preserve amplitude
 
 # DEBUG:
-verbose = True
+verbose = False
 debug = False
+makeplot = True
 
 #
 # read parameters from macro
@@ -108,7 +110,6 @@ def readCsv(fname,N=-1): # assume 1 header line
         x = newx[:newN]
         y = newy[:newN]
     return x,y
-
 #
 # process all csv files
 #
@@ -116,6 +117,11 @@ if len(sys.argv) <= 1:
     plots = [times[-1]]
 else:
     plots = [ float(val) for val in sys.argv[1:] ]
+
+if makeplot: 
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    #fig.subplots_adjust(bottom=0.2) # defaults: bottom=0.1
 
 for selectedTime in plots:
     itime = np.nonzero(times >= selectedTime)[0][0]
@@ -127,27 +133,123 @@ for selectedTime in plots:
     # read profile data
     x,y = readCsv(fname)
     x -= x[0]
-    plt.subplot(211)
-    plt.plot(x,y)
-    N = len(x)
+    if makeplot:
+        plt.subplot(311)
+        if len(plots)==1: plt.plot(x,y,label='simulated surface')
     dx = np.mean(np.diff(x))
-    print '  dx =',dx
+    if verbose: print '  avg dx =',dx
+
+    # first smooth the signal
+    Nx = len(x)
+    ysmoo = np.zeros((Nx))
+    for i in range(Nx):
+        ist = max(0,i-NSMOO)
+        ind = min(Nx,i+NSMOO)
+        ysmoo[i] = np.mean(y[ist:ind])
+
+    # then find crests
+    dy = np.zeros(Nx)
+    xm,ym = [],[]
+#    xn,yn = [],[] # points near the min/maxima
+    crests = []
+    for i in range(1,Nx-1): # calculate first derivative from smoothed signal
+#        dy[i] = (y[i+1]-y[i-1])/(x[i+1]-x[i-1])
+        dy[i] = (ysmoo[i+1]-ysmoo[i-1])/(x[i+1]-x[i-1])
+    for i in range(1,Nx-2):
+        if not np.sign(dy[i+1]) == np.sign(dy[i]): #local min/maximum
+#            xn += [x[i],x[i+1]]
+#            yn += [y[i],y[i+1]]
+            x0 = x[i] - (x[i+1]-x[i])/(dy[i+1]-dy[i])*dy[i]
+            #y0 = y[i] + (y[i+1]-y[i])/(x[i+1]-x[i])*(x0-x[i])
+            fint = interp1d( x[i-1:i+3], y[i-1:i+3], kind='cubic' )
+            y0 = fint(x0)
+#            curv1 = (y[i+1] - 2*y[i]   + y[i-1]) / np.mean( np.diff(x[i-1:i+2]) )
+#            curv2 = (y[i+2] - 2*y[i+1] + y[i]  ) / np.mean( np.diff(x[i  :i+3]) )
+            curv1 = (ysmoo[i+1] - 2*ysmoo[i]   + ysmoo[i-1]) / np.mean( np.diff(x[i-1:i+2]) )
+            curv2 = (ysmoo[i+2] - 2*ysmoo[i+1] + ysmoo[i]  ) / np.mean( np.diff(x[i  :i+3]) )
+            if verbose:
+                print 'checking (%f,%f) (%f,%f) (%f,%f) (%f,%f)' \
+                    % ( x[i-1],y[i-1], x[i],y[i], x[i+1],y[i+1], x[i+2],y[i+2] )
+                print '  min/maximum at (%f,%f)' % (x0,y0)
+                print '  curvatures',curv1,curv2
+            assert( x0 >= x[i] and x0 <= x[i+1] )
+            #assert( np.sign(curv1) == np.sign(curv2) )
+            if not np.sign(curv1) == np.sign(curv2):
+                if np.abs(curv1) > np.abs(curv2): curv = curv1
+                else: curv = curv2
+                if verbose: print '  using curv=',curv
+            else: curv = curv1
+            if curv < 0: 
+                xm.append(x0)
+                ym.append(y0)
+                crests.append(i)
+
+    ist = crests[0]
+    ind = crests[periodsToCompare]
+
+    if makeplot and len(plots)==1:
+#        plt.plot(x[ist:ind],y[ist:ind],'k-',linewidth=3)
+        plt.plot(x,ysmoo,'g--',label='smoothed signal')
+        plt.plot(xm,ym,'r+',label='detected maxima')
+#        plt.plot(xn,yn,'g.')
+
+    # build signal at equally spaced intervals
+    def nextpow2(Nx):
+        i = np.log2(Nx)
+        N = np.ceil(i)
+        return int(2**N)
+    Nfft = nextpow2(Nx)
+    xfft = np.linspace(x[ist],x[ind-1],Nfft)
+    dx = xfft[1] - xfft[0]
+    fint = interp1d( x, y, kind='cubic' )
+    yfft = fint( xfft )
+    if makeplot:
+        if len(plots)==1:
+            plt.plot(xfft,yfft,'k.',label='FFT input signal')
+            plt.legend(loc='best')
+        else:
+            plt.plot(xfft,yfft)
 
     # perform fft
-    F = fft.fft(y)/N
-    wn = fft.fftfreq(N,dx) # wavenumber
+    #F = fft.fft(y)/N
+    #F = fft.fft(y[ist:ind])/N
+    F = fft.fft(yfft) / Nfft
+    wn = fft.fftfreq(Nfft,dx) # wavenumber
+    wn *= L #normalize
     P = np.abs(F)**2 # spectral power
+    if verbose: 
+        print 'N =',Nx,' ( nextpow2:',Nfft,')'
+    print 't=',t,':  peak spatial frequency * (2pi/k) =',wn[np.argmax(P[:Nfft/2])]
 
-    # plot result
-    plt.subplot(212)
-    plt.plot(wn[:N/2]/k,P[:N/2],label='t=%f'%(t))
+    if makeplot:
+        #lidx = np.nonzero(wn > 5)[0][0]
+        plt.subplot(312)
+        #plt.plot(wn[:lidx],P[:lidx],label='t=%.2f'%(t))
+        plt.plot(wn[:Nfft/2],P[:Nfft/2],label='t=%.2f'%(t))
+        plt.subplot(313)
+        plt.loglog(wn[:Nfft/2],P[:Nfft/2],label='t=%.2f'%(t))
 
-plt.subplot(211)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.subplot(212)
-plt.xlabel('k / k_1')
-plt.ylabel('|F{y}|^2')
-plt.legend(loc='best')
+if makeplot:
+    plt.subplot(311)
+    plt.xlabel('x')
+    plt.ylabel('z(t)')
 
-plt.show()
+    plt.subplot(312)
+    plt.xlabel('k / k_1')
+    plt.ylabel('|F{z}|^2')
+    plt.legend(loc='best')
+    plt.xlim((0,6))
+    #plt.ylim((0,0.01))
+
+    plt.subplot(313)
+    plt.xlabel('k / k_1')
+    plt.ylabel('|F{z}|^2')
+    plt.xlim((0.1,20))
+    plt.ylim((1e-12,0.1))
+
+    #plt.tight_layout() # not in version of matplotlib on peregrine
+
+    plt.savefig('fft.png')
+
+    if len(plots)==1: plt.show()
+